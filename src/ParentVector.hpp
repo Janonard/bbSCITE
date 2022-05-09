ee4cf84fa3fad17ed0f9fe7d3664231c8ba9114b
Jan-Oliver "Janonard" Opdenhövel <joo@mail.uni-paderborn.de>
2022-05-09 15:05:56 +0200

    Cleaning the parent vector

--- src/ParentVector.hpp
+++ src/ParentVector.hpp
@@ -1,36 +1,34 @@
-#include "StackVector.hpp"
+#include <array>
+#include <bit>
+#include <cstdint>
+#include <sycl/ext/intel/ac_types/ac_int.hpp>
 
 namespace ffSCITE {
 template <uint64_t max_n_nodes> class ParentVector {
 public:
   static constexpr uint64_t n_node_bits = std::bit_width(max_n_nodes);
   using uindex_node_t = ac_int<n_node_bits, false>;
-  using StackVectorImpl = StackVector<uindex_node_t, max_n_nodes>;
 
-  ParentVector(uindex_node_t n_nodes) : parent() {
-    for (uindex_node_t i = 0; i < n_nodes; i++) {
-      parent.push_back(n_nodes);
+  static constexpr uindex_node_t root = max_n_nodes;
+
+  ParentVector(uindex_node_t n_nodes) : parent(), n_nodes(n_nodes) {
+    for (uindex_node_t i = 0; i < max_n_nodes; i++) {
+      parent[i] = max_n_nodes;
     }
   }
 
-  uindex_node_t const &operator[](uindex_node_t node_i) const {
+  uindex_node_t operator[](uindex_node_t node_i) const {
     return parent[node_i];
   }
 
-  uindex_node_t &operator[](uindex_node_t node_i) { return parent[node_i]; }
-
   uindex_node_t get_n_nodes() const { return parent.get_n_elements(); }
-
-  bool is_root(uindex_node_t node_i) const {
-    return parent[node_i] >= get_n_nodes();
-  }
 
   bool is_descendant(uindex_node_t node_a_i, uindex_node_t node_b_i) const {
     if (node_a_i == node_b_i) {
       return true;
     }
 
-    while (!(node_a_i == node_b_i || is_root(node_a_i))) {
+    while (!(node_a_i == node_b_i || node_a_i == root)) {
       node_a_i = parent[node_a_i];
     }
 
@@ -38,32 +36,41 @@ public:
   }
 
   void swap_nodes(uindex_node_t node_a_i, uindex_node_t node_b_i) {
-    StackVectorImpl new_a_children, new_b_children;
+    if (node_a_i == node_b_i) {
+      return;
+    }
 
     for (uindex_node_t i = 0; i < max_n_nodes; i++) {
-      if (i < get_n_nodes()) {
+      if (i != node_a_i && i != node_b_i) {
         if (parent[i] == node_a_i) {
-          new_b_children.push_back(i);
-        }
-        if (parent[i] == node_b_i) {
-          new_a_children.push_back(i);
+          parent[i] = node_b_i;
+        } else if (parent[i] == node_b_i) {
+          parent[i] = node_a_i;
         }
       }
     }
 
-    for (uindex_node_t i = 0; i < max_n_nodes; i++) {
-      if (i < new_a_children.get_n_elements()) {
-        parent[new_a_children[i]] = node_a_i;
-      }
-      if (i < new_b_children.get_n_elements()) {
-        parent[new_b_children[i]] = node_b_i;
-      }
+    if (node_a_i == parent[node_b_i]) {
+      parent[node_b_i] = parent[node_a_i];
+      parent[node_a_i] = node_b_i;
+    } else if (node_b_i == parent[node_a_i]) {
+      parent[node_a_i] = parent[node_b_i];
+      parent[node_b_i] = node_a_i;
+    } else {
+      std::swap(parent[node_a_i], parent[node_b_i]);
     }
+  }
 
-    std::swap(parent[node_a_i], parent[node_b_i]);
+  void move_node(uindex_node_t node_i, uindex_node_t new_parent_i) {
+#if __SYCL_DEVICE_ONLY__ == 0
+    assert(!is_descendant(new_parent_i, node_i));
+#endif
+
+    parent[node_i] = new_parent_i;
   }
 
 private:
-  StackVectorImpl parent;
+  std::array<uindex_node_t, max_n_nodes> parent;
+  uindex_node_t n_nodes;
 };
 } // namespace ffSCITE
--- test/ParentVector.cpp
+++ test/ParentVector.cpp
@@ -3,124 +3,106 @@
 
 using PV = ffSCITE::ParentVector<16>;
 
-TEST_CASE("ParentVector: Elementwise access", "[ParentVector]") {
-  PV pv(16);
-  for (uint64_t i = 0; i < 16; i++) {
-    pv[i] = i + 1;
-  }
-
-  for (uint64_t i = 0; i < 16; i++) {
-    REQUIRE(pv[i] == i + 1);
-  }
-}
-
 PV build_test_tree() {
   /*
-   * Build the following tree:
+   * Initial tree:
    *
-   *  ┌3┐
-   * ┌2┐4
+   * ┌-┌r┐-┐
+   * 0 1 2 3
+   */
+  PV pv(4);
+  REQUIRE(pv[0] == PV::root);
+  REQUIRE(pv[1] == PV::root);
+  REQUIRE(pv[2] == PV::root);
+  REQUIRE(pv[3] == PV::root);
+
+  /*
+   * Actual testing tree:
+   *
+   *  ┌r┐
+   * ┌2┐3
    * 0 1
    */
-  PV pv(5);
-  pv[0] = 2;
-  pv[1] = 2;
-  pv[2] = 3;
-  pv[4] = 3;
-  pv[3] = 5;
+  pv.move_node(0, 2);
+  pv.move_node(1, 2);
+  
+  REQUIRE(pv[0] == 2);
+  REQUIRE(pv[1] == 2);
+  REQUIRE(pv[2] == PV::root);
+  REQUIRE(pv[3] == PV::root);
+
   return pv;
 }
 
 TEST_CASE("ParentVector: Ancestry Queries", "[ParentVector]") {
   PV pv = build_test_tree();
 
-  for (uint64_t i = 0; i < 5; i++) {
-    if (i == 3) {
-      REQUIRE(pv.is_root(i));
-    } else {
-      REQUIRE(!pv.is_root(i));
-    }
-  }
-
   REQUIRE(pv.is_descendant(0, 0));
   REQUIRE(!pv.is_descendant(0, 1));
   REQUIRE(pv.is_descendant(0, 2));
-  REQUIRE(pv.is_descendant(0, 3));
-  REQUIRE(!pv.is_descendant(0, 4));
+  REQUIRE(!pv.is_descendant(0, 3));
 
   REQUIRE(!pv.is_descendant(1, 0));
   REQUIRE(pv.is_descendant(1, 1));
   REQUIRE(pv.is_descendant(1, 2));
-  REQUIRE(pv.is_descendant(1, 3));
-  REQUIRE(!pv.is_descendant(1, 4));
+  REQUIRE(!pv.is_descendant(1, 3));
 
   REQUIRE(!pv.is_descendant(2, 0));
   REQUIRE(!pv.is_descendant(2, 1));
   REQUIRE(pv.is_descendant(2, 2));
-  REQUIRE(pv.is_descendant(2, 3));
-  REQUIRE(!pv.is_descendant(2, 4));
+  REQUIRE(!pv.is_descendant(2, 3));
 
   REQUIRE(!pv.is_descendant(3, 0));
   REQUIRE(!pv.is_descendant(3, 1));
   REQUIRE(!pv.is_descendant(3, 2));
   REQUIRE(pv.is_descendant(3, 3));
-  REQUIRE(!pv.is_descendant(3, 4));
-
-  REQUIRE(!pv.is_descendant(4, 0));
-  REQUIRE(!pv.is_descendant(4, 1));
-  REQUIRE(!pv.is_descendant(4, 2));
-  REQUIRE(pv.is_descendant(4, 3));
-  REQUIRE(pv.is_descendant(4, 4));
 }
 
 TEST_CASE("ParentVector::swap_nodes", "[ParentVector]") {
   PV pv = build_test_tree();
 
   // Identity operation
-  pv.swap_nodes(3, 3);
+  pv.swap_nodes(2, 2);
 
   /*
    * Expected tree:
    *
-   *  ┌3┐
-   * ┌2┐4
+   *  ┌r┐
+   * ┌2┐3
    * 0 1
    */
   REQUIRE(pv[0] == 2);
   REQUIRE(pv[1] == 2);
-  REQUIRE(pv[2] == 3);
-  REQUIRE(pv[3] >= 5);
-  REQUIRE(pv[4] == 3);
+  REQUIRE(pv[2] == PV::root);
+  REQUIRE(pv[3] == PV::root);
 
   // Swap of unrelated nodes
-  pv.swap_nodes(2, 4);
+  pv.swap_nodes(2, 3);
 
   /*
    * Expected tree:
    *
-   *  ┌3┐
-   * ┌4┐2
+   *  ┌r┐
+   * ┌3┐2
    * 0 1
    */
-  REQUIRE(pv[0] == 4);
-  REQUIRE(pv[1] == 4);
-  REQUIRE(pv[2] == 3);
-  REQUIRE(pv[3] >= 5);
-  REQUIRE(pv[4] == 3);
+  REQUIRE(pv[0] == 3);
+  REQUIRE(pv[1] == 3);
+  REQUIRE(pv[2] == PV::root);
+  REQUIRE(pv[3] == PV::root);
 
   // Swap of parent and child
-  pv.swap_nodes(0, 4);
+  pv.swap_nodes(0, 3);
 
   /*
    * Expected tree:
    *
-   *  ┌3┐
+   *  ┌r┐
    * ┌0┐2
-   * 4 1
+   * 3 1
    */
-  REQUIRE(pv[0] == 3);
+  REQUIRE(pv[0] == PV::root);
   REQUIRE(pv[1] == 0);
-  REQUIRE(pv[2] == 3);
-  REQUIRE(pv[3] >= 5);
-  REQUIRE(pv[4] == 0);
+  REQUIRE(pv[2] == PV::root);
+  REQUIRE(pv[3] == 0);
 }
