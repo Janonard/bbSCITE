13b15177b1db1e7d3daca5ecf7944cb7e7454626
Jan-Oliver "Janonard" Opdenhövel <joo@mail.uni-paderborn.de>
2022-06-28 11:19:21 +0200

    Extending the benchmark script

--- src/main.cpp
+++ src/main.cpp
@@ -23,11 +23,11 @@
 
 using namespace ffSCITE;
 
-constexpr uint64_t max_n_cells = 16;
-constexpr uint64_t max_n_genes = 16;
+constexpr uint64_t max_n_cells = 64;
+constexpr uint64_t max_n_genes = 64;
 
 #ifdef EMULATOR
-using URING = oneapi::dpl::minstd_rand0;
+using URNG = oneapi::dpl::minstd_rand0;
 #else
 using URNG = DummyRNG;
 #endif 
