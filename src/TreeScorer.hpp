2c1d1ecfed26bb31d770bf3c06c35f825a9a71f2
Jan-Oliver "Janonard" Opdenhövel <joo@mail.uni-paderborn.de>
2022-07-22 17:40:34 +0200

    Triggering the generation of longer mutation data words

--- src/TreeScorer.hpp
+++ src/TreeScorer.hpp
@@ -110,8 +110,9 @@ public:
         beta_mean;
     bpriorb = bpriora * ((1 / beta_mean) - 1);
 
-    [[intel::loop_coalesce]] for (uint32_t cell_i = 0; cell_i < max_n_cells;
+    for (uint32_t cell_i = 0; cell_i < max_n_cells;
                                   cell_i++) {
+      #pragma unroll
       for (uint32_t gene_i = 0; gene_i < max_n_genes; gene_i++) {
         if (cell_i < n_cells && gene_i < n_genes) {
           this->data[cell_i][gene_i] = data_ac[cell_i][gene_i];
