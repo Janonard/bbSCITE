265a92ae1878926916c12e0d1d5963ef4458c782
Jan-Oliver "Janonard" Opdenhövel <joo@mail.uni-paderborn.de>
2022-08-11 09:46:22 +0200

    Synthesizing with high effort

--- scripts/synthesis.sh
+++ scripts/synthesis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH -A hpc-lco-kenter --mail-type=All --mail-user=joo@mail.upb.de -J ffscite-synthesis
-#SBATCH -p normal -q fpgasynthesis --time=1-00:00:00 --cpus-per-task=8 --mem=200G
+#SBATCH -p normal -q fpgasynthesis --time=2-00:00:00 --cpus-per-task=8 --mem=200G
 
 ml fpga devel intel/oneapi bittware/520n Boost/1.74.0-GCC CMake
 mkdir -p build
@@ -8,5 +8,5 @@ cd build
 
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j8 scite
-dpcpp -fintelfpga -qactypes -DHARDWARE -Xshardware -Xsv -Xsparallel=8 -Xsprofile -reuse-exe=./ffSCITE ../src/main.cpp -o ffSCITE
+dpcpp -fintelfpga -qactypes -DHARDWARE -Xshardware -Xsv -Xsparallel=8 -Xsprofile -Xshigh-effort -reuse-exe=./ffSCITE ../src/main.cpp -o ffSCITE
 tar -caf build.tar.gz scite ffSCITE ffSCITE.prj/reports
