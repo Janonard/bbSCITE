3bcbe8d13f99e50dae697e6df123d7e910db4b9f
Jan-Oliver "Janonard" Opdenhövel <joo@mail.uni-paderborn.de>
2022-09-08 14:28:42 +0200

    Extending the performance benchmark to different input sizes

---
+++ scripts/performance_benchmark/scite.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#SBATCH -A hpc-lco-kenter
+#SBATCH -p normal
+#SBATCH --mem 8G
+#SBATCH --cpus-per-task 1
+#SBATCH --time=12:00:00
+#SBATCH --mail-type=All
+#SBATCH --mail-user=joo@mail.upb.de
+
+set -e 
+
+ml fpga devel intel/oneapi bittware/520n Boost/1.74.0-GCC CMake
+
+BASE_DIR=performance_benchmark.out
+ALPHA=6e-5
+BETA=0.42
+MISSING=0.25
+
+for CELLS in 16 32 64
+do
+    GENES=$(($CELLS-1))
+
+    OUT_DIR=$BASE_DIR/$CELLS
+    FFSCITE_DIR=$OUT_DIR/ffSCITE
+    SCITE_DIR=$OUT_DIR/SCITE
+    mkdir -p $FFSCITE_DIR $SCITE_DIR
+
+    INPUT=$OUT_DIR/input.csv
+
+    for N_CHAINS in 6 12
+    do
+        for N_STEPS in `seq 500000 500000 2000000`
+        do
+            for i in `seq 10`
+            do
+                ./build/scite \
+                    -n $GENES -m $CELLS -i $INPUT -r $N_CHAINS -l $N_STEPS -fd $ALPHA -ad $BETA -max_treelist_size 1 \
+                    >> "${SCITE_DIR}/${N_CHAINS}_${N_STEPS}.log"
+            done
+        done
+    done
+done
