940e1762a074921d6ff29fa7173f2241870665a2
Jan-Oliver "Janonard" Opdenhövel <joo@mail.uni-paderborn.de>
2023-02-08 14:46:24 +0100

    Updating module versions

--- scripts/synthesis.sh
+++ scripts/synthesis.sh
@@ -2,13 +2,11 @@
 #SBATCH -A hpc-lco-kenter --mail-type=All --mail-user=joo@mail.upb.de -J ffscite-synthesis
 #SBATCH -p normal -q fpgasynthesis --time=2-00:00:00 --cpus-per-task=8 --mem=200G
 
-ml fpga devel intel/oneapi bittware/520n Boost/1.74.0-GCC CMake
+ml fpga devel intel/oneapi/22.3.0 bittware/520n Boost CMake
 mkdir -p build
 cd build
 
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j8 scite
-dpcpp -fintelfpga -qactypes -DHARDWARE \
-    -Xshardware -Xsv -Xsparallel=8 -Xsprofile -Xshigh-effort -Xsseed=1 \
-    -reuse-exe=./ffSCITE ../src/main.cpp -o ffSCITE
+make ffSCITE
 tar -caf build.tar.gz scite ffSCITE ffSCITE.prj/reports
